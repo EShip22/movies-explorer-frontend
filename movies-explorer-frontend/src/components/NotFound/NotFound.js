cd243f619b8dd70e29bf252f7fea724335dc12f0
EShip <shipelin.e@yandex.ru>
2023-05-07 21:03:34 +0300

    test

--- movies-explorer-frontend/src/components/NotFound/NotFound.js
+++ movies-explorer-frontend/src/components/NotFound/NotFound.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
   const navigate = useNavigate();
+
   return (
     <div className="not-found">
       <p className="not-found__number">404</p>
